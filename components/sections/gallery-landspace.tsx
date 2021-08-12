4d5402f925b3f60f846049a7d7e036c3da7507db
Rahadyan <rahadyan.xc@gmail.com>
2021-08-12 16:41:41 +0700

    remove image

--- components/sections/gallery-landspace.tsx
+++ components/sections/gallery-landspace.tsx
@@ -7,7 +7,6 @@ const photos = [
   '/assets/images/g4.jpeg',
   '/assets/images/g5.jpeg',
   '/assets/images/g1.jpeg',
-  '/assets/images/g2.jpeg',
   '/assets/images/g3.jpeg',
   '/assets/images/g7.jpeg',
 ];
